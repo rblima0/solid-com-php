588d5b0d775751535b639aba7c2bb9156973504f
Rodrigo Banci <rblima0@gmail.com>
2017-02-02 17:58:24 -0200

    Classes Abertas - Projeto Base

---
+++ classes-abertas/CalculadoraDePrecos.php
@@ -0,0 +1,14 @@
+<?php 
+class CalculadoraDePrecos {
+
+    public function calcula(Compra $produto) {
+        $tabela = new TabelaDePrecoPadrao();
+        $correios = new Frete();
+
+        $desconto = $tabela->descontoPara($produto->getValor());
+        $frete = $correios->para($produto->getCidade());
+
+        return $produto->getValor() * (1-$desconto) + $frete;
+    }
+
+} ?>
